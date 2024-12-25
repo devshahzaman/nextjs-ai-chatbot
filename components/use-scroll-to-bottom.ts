d652c37f40fe3a1edb439167a6d58e8b67217b34
devshahzaman <emailshahzaman@gmail.com>
2024-12-25 12:48:42 +0500

    Update use-scroll-to-bottom.ts

--- components/use-scroll-to-bottom.ts
+++ components/use-scroll-to-bottom.ts
@@ -1,8 +1,8 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, type RefObject } from 'react';
 
 export function useScrollToBottom<T extends HTMLElement>(): [
-  React.RefObject<T>,
-  React.RefObject<T>
+  RefObject<T>,
+  RefObject<T>,
 ] {
   const containerRef = useRef<T>(null);
   const endRef = useRef<T>(null);
@@ -13,37 +13,19 @@ export function useScrollToBottom<T extends HTMLElement>(): [
 
     if (container && end) {
       const observer = new MutationObserver(() => {
-        // Check if new content is added before scrolling
-        if (
-          container.scrollHeight >
-          container.scrollTop + container.clientHeight
-        ) {
-          end.scrollIntoView({ behavior: "smooth", block: "end" });
-        }
+        end.scrollIntoView({ behavior: 'instant', block: 'end' });
       });
 
       observer.observe(container, {
         childList: true,
         subtree: true,
+        attributes: true,
+        characterData: true,
       });
 
       return () => observer.disconnect();
     }
   }, []);
-
-  useEffect(() => {
-    const container = containerRef.current;
-    const end = endRef.current;
-    if (container && end) {
-      // Check if new content is added before scrolling
-      if (
-        container.scrollHeight >
-        container.scrollTop + container.clientHeight
-      ) {
-        end.scrollIntoView({ behavior: "smooth", block: "end" });
-      }
-    }
-  }, [containerRef, endRef]);
 
   return [containerRef, endRef];
 }
