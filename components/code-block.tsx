50620d38057c0d3ab20d3026ae0cdc31ae03a183
devshahzaman <emailshahzaman@gmail.com>
2024-12-25 19:39:07 +0500

    Update code-block.tsx

--- components/code-block.tsx
+++ components/code-block.tsx
@@ -29,7 +29,7 @@ export function CodeBlock({
         {tab === 'code' && (
           <pre
             {...props}
-            className={text-sm w-full overflow-x-auto dark:bg-zinc-900 p-4 border border-zinc-200 dark:border-zinc-700 rounded-xl dark:text-zinc-50 text-zinc-900}
+            className={`text-sm w-full overflow-x-auto dark:bg-zinc-900 p-4 border border-zinc-200 dark:border-zinc-700 rounded-xl dark:text-zinc-50 text-zinc-900`}
           >
             <code className="whitespace-pre-wrap break-words">{children}</code>
           </pre>
@@ -44,7 +44,7 @@ export function CodeBlock({
   } else {
     return (
       <code
-        className={${className} text-sm bg-zinc-100 dark:bg-zinc-800 py-0.5 px-1 rounded-md}
+        className={`${className} text-sm bg-zinc-100 dark:bg-zinc-800 py-0.5 px-1 rounded-md`}
         {...props}
       >
         {children}
