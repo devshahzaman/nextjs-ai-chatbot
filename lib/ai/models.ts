4e23f7a7cf2663a716a14c83f99003591d880b8f
devshahzaman <emailshahzaman@gmail.com>
2025-07-07 13:04:24 +0500

    Updated Model Label

--- lib/ai/models.ts
+++ lib/ai/models.ts
@@ -9,23 +9,23 @@ export interface Model {
 
 export const models: Array<Model> = [
   {
-    id: 'gemini-2.0-flash-exp',
-    label: 'Gemini 2.0 Flash Experimental',
-    apiIdentifier: 'gemini-2.0-flash-exp',
-    description: 'For Advanced Reasoning Tasks',
+    id: "gemini-2.0-flash-exp",
+    label: "Chat Model",
+    apiIdentifier: "gemini-2.0-flash-exp",
+    description: "For Advanced Reasoning Tasks",
   },
   {
-    id: 'gemini-1.5-flash',
-    label: 'Gemini 1.5 Flash',
-    apiIdentifier: 'gemini-1.5-flash',
-    description: 'Great for Small Tasks.',
+    id: "gemini-1.5-flash",
+    label: "Gemini 1.5 Flash",
+    apiIdentifier: "gemini-1.5-flash",
+    description: "Great for Small Tasks.",
   },
-    {
-    id: 'gemini-1.5-pro',
-    label: 'Gemini 1.5 Pro',
-    apiIdentifier: 'gemini-1.5-pro',
-    description: 'Great for Everyday Tasks.',
+  {
+    id: "gemini-1.5-pro",
+    label: "Gemini 1.5 Pro",
+    apiIdentifier: "gemini-1.5-pro",
+    description: "Great for Everyday Tasks.",
   },
 ] as const;
 
-export const DEFAULT_MODEL_NAME: string = 'gemini-2.0-flash-exp';
+export const DEFAULT_MODEL_NAME: string = "gemini-2.0-flash-exp";
